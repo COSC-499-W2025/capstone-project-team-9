7590fb63fd4ad498358231d3a99a47fa6f545603
RavenHu001 <2419652744@qq.com>
2025-10-18 16:57:39 -0700

    is_folder function

---
+++ utils/path_analysis.py
@@ -0,0 +1,10 @@
+import os
+
+def is_folder(path: str) -> bool:
+    isFolder = (
+        # os.sep is Operating system-specific path separators
+        path.endswith(os.sep) # Determine if the path ends with path separators
+        or not
+        os.path.split(path)[1] # if path not end with extend name, it consider as folder
+    )
+    return isFolder
