3b058210a804c8a15dbc4389acd8c97f3269e789
RavenHu001 <2419652744@qq.com>
2025-10-18 16:58:31 -0700

    add commit to explain function

--- utils/path_analysis.py
+++ utils/path_analysis.py
@@ -1,6 +1,9 @@
 import os
 
 def is_folder(path: str) -> bool:
+    """
+    This function use path string to check if it is a folder or not.
+    """
     isFolder = (
         # os.sep is Operating system-specific path separators
         path.endswith(os.sep) # Determine if the path ends with path separators
