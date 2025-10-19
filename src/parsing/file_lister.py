2b38fe0facb05482106985cdd4b3f7cc8df783c8
evan-pasenau <144277122+evan-pasenau@users.noreply.github.com>
2025-10-19 14:43:05 -0700

    adding needed commenting

--- src/parsing/file_lister.py
+++ src/parsing/file_lister.py
@@ -1,6 +1,12 @@
 import os
 
 def list_files(path, level=0):
+    """
+        Recursively lists all files and folders under the specified path (in nested order)
+        :param path: start directory
+        :param level: Current level (for indentation display)
+        :return: A list of strings containing the name of each file/folder
+        """
     item = []
     with os.scandir(path) as entries:
         for entry in sorted(entries, key=lambda e: e.name):
