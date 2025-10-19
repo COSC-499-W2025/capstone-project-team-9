f61059dc2a1c88ac74ce554db54716c0b7d300a3
evan-pasenau <144277122+evan-pasenau@users.noreply.github.com>
2025-10-19 14:32:48 -0700

    Bug fix: File_lister incorect ordering

--- src/parsing/file_lister.py
+++ src/parsing/file_lister.py
@@ -1,22 +1,12 @@
 import os
 
 def list_files(path, level=0):
-    """
-        Recursively lists all files and folders under the specified path (in nested order)
-        :param path: start directory
-        :param level: Current level (for indentation display)
-        :return: A list of strings containing the name of each file/folder
-        """
     item = []
     with os.scandir(path) as entries:
-        for entry in entries:
+        for entry in sorted(entries, key=lambda e: e.name):
             if entry.is_dir():
-                # if is folder, first add its name to item
-                item.append(('  '*level)+f"[DIR] {entry.name}")
-                # recursion the sub folders
-                item.extend(list_files(entry, level+1))
+                item.append(('  ' * level) + f"[DIR] {entry.name}")
+                item.extend(list_files(entry.path, level + 1))
             else:
-                # if is a file, direct add its name to item
                 item.append(('  ' * level) + f"{entry.name}")
-    # finish recursion, return item
     return item
